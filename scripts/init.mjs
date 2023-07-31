4d72dac164b9f7b5d5631bd2b2e34ff73e9803b7
Robert McIntosh <rmcintosh@remabledesigns.com>
2023-07-31 11:01:43 -0400

    1.1.2

--- scripts/init.mjs
+++ scripts/init.mjs
@@ -12,26 +12,31 @@ import './_hooks.mjs';
 /* ─────────────── ⋆⋅☆⋅⋆ ─────────────── */
 Hooks.once('init', async () => { CORE.init(); });
 Hooks.once('ready', async () => {
-	$('html').off('drop', 'body');
 	// Temporary Patch for Dice Tray and Window Tabs
-	document.body.addEventListener('drop', function (event) {
-		// Define Container for DropData
-		let dropData = null;
+	if (game.modules.get('dice-calculator')?.active) {
+		// Remove Dice Tray's Event Listener
+		$('html').off('drop', 'body');
+		
+		// Add My Replacement Event Listener
+		document.body.addEventListener('drop', function (event) {
+			// Define Container for DropData
+			let dropData = null;
 
-		// Attempt to Parse the event data
-		try {
-			dropData = JSON.parse(event.dataTransfer.getData('text/plain'));
-		} catch (err) { 
-			// Unable to Parse Data, Return Event
-			return event;
-		}
+			// Attempt to Parse the event data
+			try {
+				dropData = JSON.parse(event.dataTransfer.getData('text/plain'));
+			} catch (err) { 
+				// Unable to Parse Data, Return Event
+				return event;
+			}
 
-		// Check if dropData has a formula
-		if (!dropData?.formula) return event
+			// Check if dropData has a formula
+			if (!dropData?.formula) return event
 
-		// Create a new Roll using Formula
-		new Roll(dropData.formula).roll({async: true}).then(result => result.toMessage());
-	})
+			// Create a new Roll using Formula
+			new Roll(dropData.formula).roll({async: true}).then(result => result.toMessage());
+		})
+	}
 });
 
 //Hooks.on('renderApplication', CORE.renderSheet);
