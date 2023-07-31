a5c8b576f9f8fe9a95148cc835f525446741405c
Robert McIntosh <rmcintosh@remabledesigns.com>
2023-07-31 11:00:26 -0400

    1.1.2

--- scripts/init.mjs
+++ scripts/init.mjs
@@ -11,7 +11,28 @@ import './_hooks.mjs';
 // FOUNDRY HOOKS -> READY
 /* ─────────────── ⋆⋅☆⋅⋆ ─────────────── */
 Hooks.once('init', async () => { CORE.init(); });
-Hooks.once('ready', async () => { });
+Hooks.once('ready', async () => {
+	$('html').off('drop', 'body');
+	// Temporary Patch for Dice Tray and Window Tabs
+	document.body.addEventListener('drop', function (event) {
+		// Define Container for DropData
+		let dropData = null;
+
+		// Attempt to Parse the event data
+		try {
+			dropData = JSON.parse(event.dataTransfer.getData('text/plain'));
+		} catch (err) { 
+			// Unable to Parse Data, Return Event
+			return event;
+		}
+
+		// Check if dropData has a formula
+		if (!dropData?.formula) return event
+
+		// Create a new Roll using Formula
+		new Roll(dropData.formula).roll({async: true}).then(result => result.toMessage());
+	})
+});
 
 //Hooks.on('renderApplication', CORE.renderSheet);
 Hooks.on('renderApplication', CORE.renderSheet);
